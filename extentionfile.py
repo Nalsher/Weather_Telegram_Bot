c68203c617209aeedd33dc2cef973a0ab3b44867
nalsher <nalsher228@gmail.com>
2024-03-19 13:12:37 +0700

    last commit

--- extentionfile.py
+++ extentionfile.py
@@ -1,13 +1,15 @@
 import redis
 
+
 def redis_conn():
-    rclient = redis.Redis(host='localhost', port=6379, db=0)
+    rclient = redis.Redis(host='localhost', port=6379, db=0,decode_responses=True,charset='utf-8')
     return rclient
-def redis_end():
-    rcon = redis_conn()
-    return rcon.close()
 def redis_work(name,value):
     rd = redis_conn()
     rd.set(name=name,value=value)
-    rcon = redis_end()
-
+    rd.close()
+def redis_get(name):
+    rd = redis_conn()
+    abc = rd.get(name=name)
+    rd.close()
+    return abc
--- main.py
+++ main.py
@@ -1,20 +1,23 @@
 import telebot as tb
 from telebot import types
 from requestApi import Get_W_Api,Parse_data
-from extentionfile import redis_work
+from extentionfile import *
 
 
 
-token = '7055015230:AAG81S6EgQkZVQaWls2Y3utZrQYYms6Ze7w'
+token = 'Telegrambottokenexample'
 
 bot = tb.TeleBot(token)
 
 button_for_city = types.InlineKeyboardButton('Указать город',callback_data=None)
+button_for_check = types.InlineKeyboardButton('Выбрать последний указанный город',callback_data=None)
+button_for_favor = types.InlineKeyboardButton('Выбрать избранный город',callback_data=None)
+
 
 keyboard = types.ReplyKeyboardMarkup()
-keyboard.add(button_for_city)
+keyboard.add(button_for_favor,button_for_city,button_for_check)
 
-@bot.message_handler(commands=['start'])
+@bot.message_handler(content_types = ['text'],commands=['start'])
 
 def start(message):
     bot.send_message(chat_id=message.chat.id, text='Выберите действие', reply_markup=keyboard)
@@ -30,13 +33,34 @@ def start(message):
                 weath = Parse_data(weather)
                 chek = weath.check()
                 if chek == True:
+                    redis_work(message.from_user.username, city)
                     temper = weath.Get_Weather_temperature()
                     descrip = weath.Get_Weather_descrip()
-                    bot.send_message(chat_id=message.chat.id, text=f'Погода : {descrip}\nТемпература : {temper}')
-                    bot.send_message(chat_id=message.chat.id, text=chek)
+                    bot.send_message(chat_id=message.chat.id, text=f'Город : {city}\nПогода : {descrip}\nТемпература : {temper}')
                 else:
                     bot.send_message(chat_id=message.chat.id, text='Город не найден')
             bot.register_next_step_handler(message,returnweather)
+        if message.text == 'Выбрать последний указанный город':
+            rdget = redis_get(message.from_user.username)
+            weather = Parse_data(Get_W_Api(rdget))
+            chek = weather.check()
+            if chek == True:
+                temper = weather.Get_Weather_temperature()
+                descrip = weather.Get_Weather_descrip()
+                bot.send_message(chat_id=message.chat.id,
+                                 text=f'Город : {rdget}\nПогода : {descrip}\nТемпература : {temper}')
+        if message.text == 'Выбрать избранный город':
+            rdget = redis_get('Favour'+message.from_user.username)
+            if rdget == 'None':
+                raise KeyError
+            weather = Parse_data(Get_W_Api(rdget))
+            chek = weather.check()
+            if chek == True:
+                temper = weather.Get_Weather_temperature()
+                descrip = weather.Get_Weather_descrip()
+                bot.send_message(chat_id=message.chat.id,
+                                 text=f'Город : {rdget}\nПогода : {descrip}\nТемпература : {temper}')
+    bot.register_next_step_handler(message,send_welcome)
 
 @bot.message_handler(func=lambda message: True,commands=['save'])
 def save(message):
@@ -45,9 +69,9 @@ def save(message):
     @bot.message_handler(func=lambda message: True,content_types=['text'])
     def save_city(message):
         city = message.text
-        redis_work(message.from_user.username,city)
-    bot.register_next_step_handler(message,save_city)
-    
+        redis_work('Favour'+message.from_user.username,value=message.text)
+
 
 if __name__ == '__main__':
     bot.infinity_polling()
+
